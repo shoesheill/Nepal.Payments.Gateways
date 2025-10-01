d66609732f18ce0cbe882b85da7a8e607bd1e97a
sushil <sushil@asec.jp>
2025-10-01 14:41:24 +0900

    chore: remove unused codes only

--- src/Services/Khalti/V1/PaymentService.cs
+++ src/Services/Khalti/V1/PaymentService.cs
@@ -7,21 +7,11 @@ using Nepal.Payments.Gateways.Interfaces;
 
 namespace Nepal.Payments.Gateways.Services.Khalti.V1
 {
-    /// <summary>
-    /// Khalti payment service implementation for API version 1.
-    /// Note: V1 is not fully implemented by Khalti, this is a placeholder.
-    /// </summary>
     public class PaymentService : IPaymentService
     {
         private readonly string _secretKey;
         private readonly PaymentMode _paymentMode;
         private readonly ApiService _apiService;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PaymentService"/> class.
-        /// </summary>
-        /// <param name="secretKey">The secret key for Khalti.</param>
-        /// <param name="paymentMode">The payment mode (sandbox or production).</param>
         public PaymentService(string secretKey, PaymentMode paymentMode)
         {
             _secretKey = secretKey ?? throw new ArgumentNullException(nameof(secretKey));
@@ -29,29 +19,10 @@ namespace Nepal.Payments.Gateways.Services.Khalti.V1
             _apiService = new ApiService(new HttpClient());
         }
 
-        /// <summary>
-        /// Initiates a payment transaction asynchronously for Khalti V1.
-        /// Note: Khalti V1 is not fully supported, this method throws NotImplementedException.
-        /// </summary>
-        /// <typeparam name="T">The type of response expected.</typeparam>
-        /// <param name="content">The payment request content.</param>
-        /// <param name="version">The payment gateway API version.</param>
-        /// <returns>A task that represents the asynchronous operation and contains the response.</returns>
-        /// <exception cref="NotImplementedException">Thrown because Khalti V1 is not supported.</exception>
         public Task<T> InitiatePaymentAsync<T>(object content, PaymentVersion version)
         {
             throw new NotImplementedException("Khalti V1 API is not supported. Please use Khalti V2 API.");
         }
-
-        /// <summary>
-        /// Verifies a payment transaction asynchronously for Khalti V1.
-        /// Note: Khalti V1 is not fully supported, this method throws NotImplementedException.
-        /// </summary>
-        /// <typeparam name="T">The type of response expected.</typeparam>
-        /// <param name="content">The payment verification content.</param>
-        /// <param name="version">The payment gateway API version.</param>
-        /// <returns>A task that represents the asynchronous operation and contains the verification response.</returns>
-        /// <exception cref="NotImplementedException">Thrown because Khalti V1 is not supported.</exception>
         public Task<T> VerifyPaymentAsync<T>(string content, PaymentVersion version)
         {
             throw new NotImplementedException("Khalti V1 API is not supported. Please use Khalti V2 API.");
